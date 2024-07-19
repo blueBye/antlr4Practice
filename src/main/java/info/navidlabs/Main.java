697c023e9e621c9bfab514fb766995d4994b8f6f
blueBye <navidshirmohammadi81@gmail.com>
2024-07-19 17:39:23 +0330

    fixed some bugs from branch L02-labeled-expression-language

--- src/main/java/info/navidlabs/Main.java
+++ src/main/java/info/navidlabs/Main.java
@@ -1,17 +1,26 @@
 package info.navidlabs;
 
-import info.navidlabs.L02LabeledExpressionLanguage.TestGrammar;
 
 public class Main {
     public static void main(String[] args) {
         if (args.length == 0) {return;}
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        String inputFile = null;
 
         switch (args[0]) {
+            case "l01":
+                inputFile = classloader.getResource("test-files/l01-t01.txt").getPath();
+                info.navidlabs.L01ExpresionLanguage.TestGrammar g01 =
+                        new info.navidlabs.L01ExpresionLanguage.TestGrammar();
+                try{ g01.test01(inputFile); }
+                catch (Exception e) { return; }
+                break;
+
             case "l02":
-                String inputFile = classloader.getResource("test-files/l02-t01.txt").getPath();
-                TestGrammar g = new TestGrammar();
-                try{ g.test01(inputFile); }
+                inputFile = classloader.getResource("test-files/l02-t01.txt").getPath();
+                info.navidlabs.L02LabeledExpressionLanguage.TestGrammar g02 =
+                        new info.navidlabs.L02LabeledExpressionLanguage.TestGrammar();
+                try{ g02.test01(inputFile); }
                 catch (Exception e) { return; }
                 break;
         }
